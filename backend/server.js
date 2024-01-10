db2c5db68135ab21d9e3f6e33f4c98dc5470a626
Syed Zulqarnain Haider <104985218+HaiderIsBack@users.noreply.github.com>
2024-01-10 18:22:08 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const fs = require("fs")
 const http = require("https")
 
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
 const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
